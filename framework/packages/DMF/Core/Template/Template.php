4f15f6b2d4e14e8374caa1f11007d4629682bb3a
damirazo <damirazo.kazan@gmail.com>
2012-12-24 21:23:03 +0400

    Исправлена ошибка с именами констант из старой версии Исправлена ошибка с именем метода при получении классом Template глобального контекста

--- framework/packages/DMF/Core/Template/Template.php
+++ framework/packages/DMF/Core/Template/Template.php
@@ -1,19 +1,13 @@
 <?php
 
-    /**
-     * @package    DMF
-     * @subpackage Template
-     * @author     damirazo <damirazo.kazan@gmail.com>
-     * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
-     * @version    0.1.0
-     * @since      Класс для обработки шаблонов
-     */
-
     namespace DMF\Core\Template;
 
     use DMF\Core\Component\Component;
     use DMF\Core\Http\Response;
 
+    /**
+     * Базовый класс для реализации шаблонов
+     */
     class Template extends Component
     {
 
@@ -37,7 +31,6 @@
             $this->template_name = $template_name;
             $this->data = $data;
             $this->http_response_code = $http_response_code;
-
             return $this->render();
         }
 
@@ -50,22 +43,19 @@
             $debug = $this->config('debug');
             // Объект загрузчика
             $loader = new \Twig_Loader_Filesystem(
-                APPLICATION_ROOT . $this->get_module_name() . SEPARATOR . 'View'
+                APP_PATH . $this->get_module_name() . _SEP . 'View'
             );
             // Объект окружения
             $twig = new \Twig_Environment($loader, [
-                'cache' => DATA_ROOT . 'templates_cache',
+                'cache' => DATA_PATH . 'templates_cache',
                 'debug' => $debug
             ]);
-
             // Глобальные переменные
-            foreach (Context::$_data as $name => $value) {
+            foreach (Context::data() as $name => $value) {
                 $twig->addGlobal($name, $value);
             }
-
             // Возвращаем отрендеренный шаблон
             $response = $twig->render($this->template_name . '.twig', $this->data);
-
             return new Response($response, $this->http_response_code);
         }
 
