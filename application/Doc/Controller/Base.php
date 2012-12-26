17cf1997c834dfcde23fe854c4db82be42af9e74
damirazo <damirazo.kazan@gmail.com>
2012-12-26 19:01:24 +0400

    Исправлено игнорирование приложений

---
+++ application/Doc/Controller/Base.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace App\Doc\Controller;
+
+    use DMF\Core\Controller\Controller;
+
+    /**
+     * Базовый контроллер для документации
+     */
+    class Base extends Controller
+    {
+
+    }
