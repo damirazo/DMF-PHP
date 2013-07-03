70206c23778f97a9a0ff3705a346f672eb4c0f86
Damir Abdullin <me@damirazo.ru>
2013-07-03 21:09:51 +0400

    Изменение стиля отображения информации об ошибке

--- framework/packages/DMF/Core/ErrorHandler/templates/error.php
+++ framework/packages/DMF/Core/ErrorHandler/templates/error.php
@@ -4,6 +4,13 @@
     <meta charset="utf-8">
     <title><?php echo $error['code'] . ' &bull; ' . substr($error['message'], 0, 32) . '...'; ?></title>
     <style type="text/css">
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Segoe UI', 'Arial', sans-serif;
+            font-size: 14px;
+        }
+
         .error-message, {
             padding: 8px 35px 8px 14px;
             text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
@@ -14,6 +21,7 @@
             margin: 20px auto;
             text-align: center;
         }
+
         .error-message {
             color: #b94a48;
             background-color: #f2dede;
@@ -26,5 +34,6 @@
 <div class="error-message">
     <?php echo $error['message']; ?>
 </div>
+
 </body>
 </html>
